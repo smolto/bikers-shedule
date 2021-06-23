ee7dda7a6c2b871cbcc4723600791123828cba7b
Santiago Molto <sant8613@gmail.com>
2021-06-23 19:36:04 +0200

    Login implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
+import {
+  useState,
+  useEffect
+} from 'react'
+
 import '../styles/globals.css'
 
 function MyApp ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [user, setUser] = useState('')
+  const [isValidUser, setIsValidUser] = useState(false)
+
+  useEffect(() => {
+    if (localStorage.getItem('user') !== null) {
+      setUser(localStorage.getItem('user'))
+      setIsValidUser(true)
+    }
+  }, [])
+
+  return <Component
+    {...pageProps}
+    user={user}
+    setUser={setUser}
+    isValidUser={isValidUser}
+    setIsValidUser={setIsValidUser}
+  />
 }
 
 export default MyApp
